a7125052a06c7b661701ec8fe80a4ad7e64efde1
waxnet <88239635+waxnet@users.noreply.github.com>
2023-05-31 21:27:32 +0200

    update

--- pack.cmd
+++ pack.cmd
@@ -3,4 +3,5 @@
 mkdir "%userprofile%/Documents/Teardown/mods/TearHook"
 xcopy /c /y /e . "%userprofile%/Documents/Teardown/mods/TearHook"
 cd "%userprofile%/Documents/Teardown/mods/TearHook"
+rmdir /s /q media
 del pack.cmd
